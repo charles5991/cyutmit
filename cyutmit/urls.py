5791581174847cb10c364c0d4986cb11328dd65f
frank <frank@frank>
2016-03-27 23:55:40 +0800

    remove file

--- cyutmit/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('visits.urls')),
-    url(r'^visits/', include('visits.urls', namespace='visits')),
-]
